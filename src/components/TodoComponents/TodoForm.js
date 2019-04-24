ac8054a6ca515e126fd42c3cef59a8ca60556c68
Chase Wex <clapingmanly@hotmail.com>
2019-04-24 12:26:44 -0400

    Added a form that can be submited and recieved by the app

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+
+
+class TodoForm extends React.Component
+{
+
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+    
+    handleSubmit(event) {
+        this.props.callback(this.state.value);
+        event.preventDefault();
+        this.setState({value: ''});
+    }
+    handleReset(event) 
+    {
+        console.log("hello");
+        this.setState({value: ''});
+    }
+    render()
+    {
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Add Todo" />
+                <input type="reset" value="Clear" onClick={()=> this.handleReset()}/> {/* doesnt manually call the reset for some reason?*/}
+            </form>
+        );
+    }
+}
+
+export default TodoForm;

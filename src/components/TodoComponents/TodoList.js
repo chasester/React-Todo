dd3589c1fd899e02c65eb9a68eacfa3092a42ff5
Chase Wex <clapingmanly@hotmail.com>
2019-04-24 12:26:18 -0400

    Added the original logic for the todolist

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,23 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+class TodoList extends React.Component
+{
+
+    render()
+    {
+        return(
+            <div className='list-container'>
+                {this.props.todoListItems.map((x,i)=> <div className='list-item' key={i} onClick={()=> this.clicked()}>{x}</div>)}
+            </div>
+        );
+    }
+
+    clicked()
+    {
+        
+    }
+}
+
+export default TodoList;
